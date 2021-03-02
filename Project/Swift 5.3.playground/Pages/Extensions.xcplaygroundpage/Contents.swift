29744eb8e8b7517f73704c41da1b719c2d64ba49
ruiz211 <ruiz.im211@gmail.com>
2021-03-02 16:09:17 -0500

    Extensions

--- Project/Swift 5.3.playground/Pages/Extensions.xcplaygroundpage/Contents.swift
+++ Project/Swift 5.3.playground/Pages/Extensions.xcplaygroundpage/Contents.swift
@@ -1,1 +1,19 @@
+import Foundation
 
+extension Int {
+    static func factorial(number: Int) -> Int {
+        if number < 0 { return 0 }
+        else if number == 0 { return 1 }
+        return number * factorial(number: number - 1)
+    }
+    
+     mutating func square() {
+        self = self * self
+    }
+
+}
+
+print(Int.factorial(number: 3))
+var number = 3
+number.square()
+print(number)

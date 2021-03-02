e8d4fd59483c47628661c98496efdb15d01b4cf9
ruiz211 <ruiz.im211@gmail.com>
2021-03-02 14:28:29 -0500

    Type Casting

--- Project/Swift 5.3.playground/Pages/Type Casting.xcplaygroundpage/Contents.swift
+++ Project/Swift 5.3.playground/Pages/Type Casting.xcplaygroundpage/Contents.swift
@@ -1,1 +1,35 @@
+import Foundation
 
+class Person {
+    var name: String!
+    var age: Int!
+}
+
+var person: Person = Person()
+person.name = "Charles"
+person.age = 11
+
+var number: Int = 11
+var isAlive: Bool = true
+
+var types: [Any] = [person, number, isAlive]
+types.append(37.73)
+
+for item in types {
+    if item is Person {
+        let value = item as! Person
+        print("Person name \(value.name!)")
+    } else if item is String {
+        let value = item as! String
+        print("String \(value)")
+    } else if item is Int {
+        let value = item as! Int
+        print("Int \(value)")
+    } else if item is Double {
+        let value = item as! Double
+        print("Double \(value)")
+    } else if item is Bool {
+        let value = item as! Bool
+        print("Bool \(value)")
+    }
+}

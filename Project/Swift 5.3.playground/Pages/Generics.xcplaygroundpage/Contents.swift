87360c937f0f99632714fbde7504be4f72557a67
ruiz211 <ruiz.im211@gmail.com>
2021-03-03 15:47:34 -0500

    Generic Functions

--- Project/Swift 5.3.playground/Pages/Generics.xcplaygroundpage/Contents.swift
+++ Project/Swift 5.3.playground/Pages/Generics.xcplaygroundpage/Contents.swift
@@ -1,1 +1,16 @@
+import Foundation
 
+//Generic Functions
+
+func swapTwoValues<T>(a: inout T, b: inout T) -> Void {
+    let temp = a
+    a = b
+    b = temp
+}
+
+var a = 11
+var b = 1
+
+print("\(a) \(b)")
+swapTwoValues(a: &a, b: &b)
+print("\(a) \(b)")

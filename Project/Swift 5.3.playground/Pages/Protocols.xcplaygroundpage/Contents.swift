a4155549ce63cc1fab6a565bd835471ef89229a1
ruiz211 <ruiz.im211@gmail.com>
2021-03-03 14:15:09 -0500

    Protocol Syntax

--- Project/Swift 5.3.playground/Pages/Protocols.xcplaygroundpage/Contents.swift
+++ Project/Swift 5.3.playground/Pages/Protocols.xcplaygroundpage/Contents.swift
@@ -1,1 +1,41 @@
+import Foundation
+
+//Protocol Syntax
+
+protocol MathOprerations {
+    func addition(x: Int, y: Int) -> Int
+    func rest(x: Int, y: Int) -> Int
+    func product(x: Int, y: Int) -> Int
+    func division(x: Int, y: Int) -> Int
+}
+
+class Operations: MathOprerations {
+    var x: Int, y: Int
+    
+    init(x: Int, y: Int) {
+        self.x = x
+        self.y = y
+    }
+    
+    func addition(x: Int, y: Int) -> Int {
+        return x + y
+    }
+    
+    func rest(x: Int, y: Int) -> Int {
+        return x - y
+    }
+    
+    func product(x: Int, y: Int) -> Int {
+        return x * y
+    }
+    
+    func division(x: Int, y: Int) -> Int {
+        return x / y
+    }
+}
+
+let ad = Operations(x: 12, y: 43)
+print(ad.addition(x: ad.x, y: ad.y))
+
+
 

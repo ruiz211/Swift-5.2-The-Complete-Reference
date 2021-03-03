1bde9470e83e062f75e1e5c5a542f8d2c8f234a8
ruiz211 <ruiz.im211@gmail.com>
2021-03-03 16:12:58 -0500

    Generic Types

--- Project/Swift 5.3.playground/Pages/Generics.xcplaygroundpage/Contents.swift
+++ Project/Swift 5.3.playground/Pages/Generics.xcplaygroundpage/Contents.swift
@@ -14,3 +14,31 @@ var b = 1
 print("\(a) \(b)")
 swapTwoValues(a: &a, b: &b)
 print("\(a) \(b)")
+
+
+
+//Generic Types
+
+struct Stack<T> {
+    var items: [T] = [T]()
+    
+    mutating func push(item: T) -> Void {
+        items.append(item)
+    }
+    
+    mutating func pop() -> T {
+        items.removeLast()
+    }
+}
+
+var stackOfStrings = Stack<String>()
+stackOfStrings.push(item: "uno")
+stackOfStrings.push(item: "dos")
+stackOfStrings.push(item: "tres")
+stackOfStrings.push(item: "cuatro")
+
+for item in stackOfStrings.items {
+    print(item)
+}
+
+

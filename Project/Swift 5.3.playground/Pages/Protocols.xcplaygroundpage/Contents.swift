af43a41732cc14bb286ccd34574beb9735e38d83
ruiz211 <ruiz.im211@gmail.com>
2021-03-03 14:59:31 -0500

    Protocols as Types

--- Project/Swift 5.3.playground/Pages/Protocols.xcplaygroundpage/Contents.swift
+++ Project/Swift 5.3.playground/Pages/Protocols.xcplaygroundpage/Contents.swift
@@ -122,3 +122,38 @@ class RequireInit: RequireInitProtocol {
 
 
 
+//Protocols as Types
+
+protocol RandomNumberGenerator {
+    func random() -> Double
+}
+
+class Dice {
+    let sides: Int
+    let generator: RandomNumberGenerator
+    init(sides: Int, generator: RandomNumberGenerator) {
+        self.sides = sides
+        self.generator = generator
+    }
+    func roll() -> Int {
+        return Int(generator.random() * Double(sides)) + 1
+    }
+}
+
+class LinearCongruentialGenerator: RandomNumberGenerator {
+    var lastRandom = 42.0
+    let m = 139968.0
+    let a = 3877.0
+    let c = 29573.0
+    func random() -> Double {
+        lastRandom = ((lastRandom * a + c).truncatingRemainder(dividingBy: m))
+        return lastRandom / m
+    }
+}
+
+
+let roll = Dice(sides: 6, generator: LinearCongruentialGenerator())
+print(roll.roll())
+
+
+
